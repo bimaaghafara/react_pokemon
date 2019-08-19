2b0a507a07b146b1a5712f0f74321acc62a67368
bimaa.pubiyangga <bimaa.purbayangga@infolog.com.sg>
2019-08-20 00:51:13 +0700

    refactor: refactor pokemont-list page into React.compoenent
    Reafactor  pokemont-list page into React.compoenent, so we can implement infinite scroll. Added methods: handleScroll, getNextPokemons.

--- src/pages/pokemon-list.js
+++ src/pages/pokemon-list.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {Component, useState, useEffect } from 'react';
 
 // components
 import Loader from '../components/loader';
@@ -8,34 +8,108 @@ import PokemonCard from '../components/pokemon-card';
 import gql from 'graphql-tag';
 import { ApolloConsumer } from '@apollo/react-hooks';
 
+class PokemonList extends Component {
 
-function PokemonList(props) {
-    // component state: [key: setter] = useState(defaultValue);
-    const [pokemons, setPokemons] = useState([]);
-    const [loader, setLoader] = useState(true);
+    constructor(props) {
+        super(props);
+        this.state = {
+            loader: false,
+            count: 10,
+            pokemons: [],
+            nextPokemons: [],
+            fetchingNextPokemons: false,
+            endOfCatalogue: false
+        };
+        
+        this.handleScroll = this.handleScroll.bind(this);
+    };
 
-    // Similar to componentDidMount
-    useEffect(() => {
-        getPokemons(11);
-    }, []);
+    componentDidMount() {
+        // get pokemons for the first time
+        this.getPokemons(this.state.count);
+        document.addEventListener('scroll', this.handleScroll);
+    }
+    
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.handleScroll);
+    }
 
-    const getPokemons = (first) => {
-        props.client.query({
-            query: GET_POKEMONS,
+    async handleScroll() {
+        // isNearBottom = true; when user reach to 375px (about 1 product wrapper height) from bottom page
+        const isNearBottom = window.innerHeight + window.scrollY >= (document.body.offsetHeight) - 315;
+        if (isNearBottom) {
+            if (this.state.nextPokemons.length>0) {
+                // update more pokemons when nextPokemons is exist
+                await this.setState({pokemons: this.state.nextPokemons})
+
+                // reset nextPokemons after we add them, to prevent add more nextPokemons with same nextPokemons
+                await this.setState({nextPokemons: []})
+
+            } else if (!this.state.endOfCatalogue) {
+                // when user isNearBottom but app isFetchingNextPokemons, app will show loader
+                // try to scroll down so fast into isNearBottom
+                // show loader here
+                // loader will be hidden when get pokemons is done
+                this.setState({loader: true});
+            }
+        }
+
+        // getNextPokemons when showLoading=false, isFetchingNextPokemons=false, nextPokemons=[], and endOfCatalogue=false;
+        if (!this.state.loader && !this.state.fetchingNextPokemons && this.state.nextPokemons.length===0 && !this.state.endOfCatalogue) {
+            this.getNextPokemons(this.state.count);
+        }
+    }
+
+    getPokemons = (first) => {
+        this.setState({loader: true});
+        this.props.client.query({
+            query: this.GET_POKEMONS,
             variables: {first: first}
         })
         .then(data => {
-            setLoader(false);
-            setPokemons(data.data.pokemons);
+            this.setState({
+                loader: false,
+                pokemons: data.data.pokemons,
+                count: this.state.count+10
+            });
+
+            // scroll down & up window by 10px, to trigger handleScroll()
+            window.scrollBy(0, 10);
+            window.scrollBy(0, -10);
         })
         .catch(error => {
-            setLoader(false);
+            this.setState({loader: false});
+            console.error(error);
+        });
+    }
+
+    getNextPokemons = (first) => {
+        this.setState({fetchingNextPokemons: true});
+        this.tes = this.props.client.query({
+            query: this.GET_POKEMONS,
+            variables: {first: first}
+        })
+        .then(data => {
+            this.setState({
+                loader: false,
+                fetchingNextPokemons: false,
+                nextPokemons: data.data.pokemons,
+                count: this.state.count+10,
+                endOfCatalogue: data.data.pokemons.length===this.state.pokemons.length? true: false
+            });
+
+            // scroll down & up window by 10px, to trigger handleScroll()
+            window.scrollBy(0, 10);
+            window.scrollBy(0, -10);
+        })
+        .catch(error => {
+            this.setState({loader: false});
             console.error(error);
         });
     }
 
     // query fro graphql
-    const GET_POKEMONS = gql`
+    GET_POKEMONS = gql`
         query Pokemons($first: Int!) {
             pokemons(first: $first) {
                 id
@@ -48,35 +122,40 @@ function PokemonList(props) {
         }
     `;
 
-    const goToPage = (url) => {
-        props.history.push(url);
+    goToPage = (url) => {
+        this.props.history.push(url);
     }
 
-    return (
-        <div className="container">
-            <Loader visible={loader}></Loader>
-            
-            <div className="col-xs-12">
-                <h1>Pokemon List</h1>
-            </div>
-
-            {pokemons.map(pokemon =>
-                <div key={pokemon.id}>
-                    <div className="col-xs-12 col-sm-4 col-md-4 col-lg-3 well-sm">
-                        <PokemonCard
-                            id={pokemon.id}
-                            number={pokemon.number}
-                            name={pokemon.name}
-                            image={pokemon.image}
-                            classification={pokemon.classification}
-                            types={pokemon.types}
-                            onClick={() => goToPage(`/pokemon/${pokemon.id}`)}
-                        />
-                    </div>
+    render() {
+        const {loader, pokemons} = this.state;
+    
+        return (
+            <div className="container">
+                <Loader visible={loader}></Loader>
+                
+                <div className="col-xs-12">
+                    <h1>Pokemon List</h1>
                 </div>
-            )}
-        </div>
-    );
+
+                {pokemons.map(pokemon =>
+                    <div key={pokemon.id}>
+                        <div className="col-xs-12 col-sm-4 col-md-4 col-lg-3 well-sm">
+                            <PokemonCard
+                                id={pokemon.id}
+                                number={pokemon.number}
+                                name={pokemon.name}
+                                image={pokemon.image}
+                                classification={pokemon.classification}
+                                types={pokemon.types}
+                                onClick={() => this.goToPage(`/pokemon/${pokemon.id}`)}
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+
 }
 
 const PokemonListWrapper = (props) => <ApolloConsumer>
